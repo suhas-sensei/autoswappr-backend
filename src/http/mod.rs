cbf691bc5e9f1166e5c8b43082388e9f0e1ff9d0
stevencartavia <corderosteven6@gmail.com>
2024-11-27 10:19:55 -0600

    feat: Unsubscription Service

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,11 +1,14 @@
-use axum::{routing::get, Router};
+use axum::{routing::{get, post}, Router};
 mod health_check;
 mod types;
+mod unsubscription;
 
 use crate::AppState;
 
 // Application router.
 // All routes should be merged here.
 pub fn router() -> Router<AppState> {
-    Router::new().route("/health_check", get(health_check::health_check))
+    Router::new()
+        .route("/health_check", get(health_check::health_check))
+        .route("/unsubscribe", post(unsubscription::handle_unsubscribe))
 }
